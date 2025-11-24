6d4ee53a247da18e96417669c78f7479162d81c7
JNTMTMTM <15251865468@163.com>
2025-11-24 21:46:33 +0800

    V1.1.0 更新项目版本号：将setup.py中的版本号从1.0.0升级至1.1.0

--- SHICTHRSMACE/setup.py
+++ SHICTHRSMACE/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='SHICTHRSMACE',
-      version='1.0.0',
+      version='1.1.0',
       description='SHICTHRS MACE machine identity system',
       url='https://github.com/JNTMTMTM/SHICTHRS_MACE',
       author='SHICTHRS',
